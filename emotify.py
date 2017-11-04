ef7ea3938706780d8abd657945291dfbd14117d4
Anais <anaiskegels@hotmail.com>
2017-11-04 14:15:49 +0000

    change py and html for email linking

--- emotify.py
+++ emotify.py
@@ -2,17 +2,13 @@ from flask import Flask, render_template, request
 app = Flask("emotify")
 
 @app.route("/")
-def hello():
-    return "Emotify: Listen to your Emotions"
+def welcome():
+    return render_template ("emotify.html")
 
-@app.route("/<emotion>")
-def hello_someone(emotion):
-    return render_template ("emotify.html", name = emotion.title() )
-
-@app.route("/signup", methods=["POST"])
+@app.route("/", methods=["POST"])
 def sign_up():
     form_data = request.form
     print form_data["email"]
-    return "All OK"
+    return "Go check your inbox!"
 
 app.run(debug=True)
