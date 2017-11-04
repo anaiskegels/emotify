6d66b4bcae50444cc8a7ab20194d1e9154fa5cb3
Anais <anaiskegels@hotmail.com>
2017-11-04 14:27:27 +0000

    trying to figure out python

--- emotify.py
+++ emotify.py
@@ -1,14 +1,10 @@
 from flask import Flask, render_template, request
 app = Flask("emotify")
 
-@app.route("/")
-def welcome():
-    return render_template ("emotify.html")
-
 @app.route("/", methods=["POST"])
-def sign_up():
+def hello():
     form_data = request.form
     print form_data["email"]
-    return "Go check your inbox!"
+    return render_template ("emotify.html")
 
 app.run(debug=True)
